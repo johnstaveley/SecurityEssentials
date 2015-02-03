33144cf2a55cc42b12bb9bed721e8c5b344149ab
John Staveley <john@staveley.org>
2015-02-03 09:25:04 +0000

    overposting example

--- SecurityEssentials/Controllers/SecurityController.cs
+++ SecurityEssentials/Controllers/SecurityController.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SecurityEssentials.Core;
+using Microsoft.AspNet.Identity;
+using SecurityEssentials.ViewModel;
 
 namespace SecurityEssentials.Controllers
 {
 	public class SecurityController : AntiForgeryControllerBase
 	{
+
+#if DEBUG
 
 		public ActionResult InformationDisclosure()
 		{
@@ -23,9 +27,53 @@ namespace SecurityEssentials.Controllers
 			ViewBag.CssTest = "red";
 			ViewBag.HtmlTest = "<b>If unencoded, this text should appear bold</b>";
 			ViewBag.JavaScriptTest = "hello";
-//			ViewBag.JavaScriptTest = "hello'); alert('hello";
+			//			ViewBag.JavaScriptTest = "hello'); alert('hello";
 			return View();
 		}
 
+
+		#region Edit
+
+		public ActionResult OverPostingEdit(int id)
+		{
+			using (var context = new SEContext())
+			{
+				var users = context.User.Where(u => u.Id == id);
+				var currentUser = Convert.ToInt32(User.Identity.GetUserId());
+				var user = users.FirstOrDefault();
+				return View(new UserViewModel(currentUser, User.IsInRole("Admin"), user));
+			}
+		}
+
+		[HttpPost]
+		public ActionResult OverPostingEdit(int id, FormCollection collection)
+		{
+			using (var context = new SEContext())
+			{
+
+				var users = context.User.Where(u => u.Id == id);
+				if (users == null) return new HttpNotFoundResult();
+				var user = users.FirstOrDefault();
+				var currentUser = Convert.ToInt32(User.Identity.GetUserId());
+
+				var propertiesToUpdate = new List<string>()
+				//{
+				//	"FirstName", "LastName", "TelNoHome", "TelNoMobile", "TelNoWork", "Title",
+				//	"Town","Postcode", "SkypeName"
+				//}
+				;
+				if (TryUpdateModel(user, "User", propertiesToUpdate.ToArray(), collection))
+				{
+						context.SaveChanges();
+						ViewBag.Notification = "Details updated";
+						return View("OverPostingEdit");
+				}
+
+				return View(new UserViewModel(currentUser, User.IsInRole("Admin"), user));
+			}
+		}
+
+		#endregion
 	}
+#endif
 }
