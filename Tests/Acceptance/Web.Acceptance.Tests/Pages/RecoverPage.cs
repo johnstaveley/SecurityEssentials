a6d0d880dbea5a8243c8b574496c17234ad080b8
johnstaveley <johnstaveley>
2016-07-02 05:55:24 +0100

    added missing page

---
+++ Tests/Acceptance/Web.Acceptance.Tests/Pages/RecoverPage.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using SecurityEssentials.Acceptance.Tests.Web.Menus;
+using TechTalk.SpecFlow;
+
+namespace SecurityEssentials.Acceptance.Tests.Web.Pages
+{
+	public class RecoverPage : BasePage
+	{
+
+		private IWebElement UserName
+		{
+			get { return this.GetVisibleWebElement(By.Id("UserName")); }
+		}
+
+		private IWebElement RecoverButton
+		{
+			get { return this.GetVisibleWebElement(By.Id("recoverButton")); }
+		}
+
+		public RecoverPage(IWebDriver webDriver, Uri baseUri)
+			: base(webDriver, baseUri, PageTitles.RECOVER_PASSWORD)
+		{
+		}
+
+
+		public void EnterDetails(Table table)
+		{
+
+			foreach (var row in table.Rows)
+			{
+				switch (row[0])
+				{
+					case "Username":
+						UserName.SendKeys(row[1]);
+						break;
+					default:
+						throw new Exception(string.Format("Field {0} not defined", row[0]));
+				}
+			}
+		}
+
+		public void ClickSubmit()
+		{
+			RecoverButton.Click();
+		}
+
+	}
+
+}
