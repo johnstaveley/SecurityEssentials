432cc4d4a2bb1916018b5fc00dd6df3a051ef5ba
johnstaveley <johnstaveley>
2016-07-25 06:45:20 +0100

    Put in Proxy of acceptance tests so that they can be run through OWASP Zap

--- Tests/Acceptance/Web.Acceptance.Tests/Steps/Hooks.cs
+++ Tests/Acceptance/Web.Acceptance.Tests/Steps/Hooks.cs
@@ -6,6 +6,7 @@ using OpenQA.Selenium.Support.Extensions;
 using TechTalk.SpecFlow;
 using SecurityEssentials.Acceptance.Tests.Web.Extensions;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
 
 namespace SecurityEssentials.Acceptance.Tests.Web.Steps
 {
@@ -17,7 +18,21 @@ namespace SecurityEssentials.Acceptance.Tests.Web.Steps
 		public static void BeforeFeature()
 		{
 
-            IWebDriver webDriver = new FirefoxDriver();
+			var webBrowserProxy = ConfigurationManager.AppSettings["WebBrowserProxy"].ToString();
+			IWebDriver webDriver = null;
+			if (!string.IsNullOrEmpty(webBrowserProxy))
+			{
+				var proxy = new Proxy();
+				proxy.HttpProxy = webBrowserProxy;
+				proxy.FtpProxy = webBrowserProxy;
+				proxy.SslProxy = webBrowserProxy;
+				var capabilities = new DesiredCapabilities();
+				capabilities.SetCapability(CapabilityType.Proxy, proxy);
+				webDriver = new FirefoxDriver(capabilities);
+			}
+			else {
+				webDriver = new FirefoxDriver();
+			}
 			webDriver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 5));
 			FeatureContext.Current.SetWebDriver(webDriver);
 
