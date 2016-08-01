d46f51ed73f99a658ddf0426b899829a739c65be
johnstaveley <johnstaveley>
2016-08-01 06:41:39 +0100

    Turned off auto-complete on logon form , removed vulnerable header

--- SecurityEssentials/Core/HttpHeaders.cs
+++ SecurityEssentials/Core/HttpHeaders.cs
@@ -17,11 +17,13 @@ namespace SecurityEssentials.Core
 
 		void context_PreSendRequestHeaders(object sender, EventArgs e)
 		{
-			HttpContext.Current.Response.Headers.Add("X-Frame-Options", "Deny"); // Prevent site being viewed in an iFrame
+			HttpContext.Current.Response.Headers.Add("X-Frame-Options", "Deny"); // SECURE: Prevent site being viewed in an iFrame
+			HttpContext.Current.Response.Headers.Add("X-Content-Type-Options", "nosniff"); // SECURE: Prevent site being displayed in a different format in older browsers c.f. https://www.owasp.org/index.php/List_of_useful_HTTP_headers
+			HttpContext.Current.Response.Headers.Add("X-XSS-Protection", "1; mode=block"); // SECURE: Enable browsers anti-XSS protection https://www.owasp.org/index.php/XSS_(Cross_Site_Scripting)_Prevention_Cheat_Sheet
 #if !DEBUG
 			HttpContext.Current.Response.AddHeader("Strict-Transport-Security", "max-age=31536000"); // Ensure all future requests to the site are made over SSL
 #endif
-	        HttpContext.Current.Response.Headers.Remove("Server"); // Remove server information disclosure
+			HttpContext.Current.Response.Headers.Remove("Server"); // Remove server information disclosure
 		}
 
 		public void Dispose()
