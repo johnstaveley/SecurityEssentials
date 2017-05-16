493fee34b3f8091a15a13eae4210d28e0bcd0278
the f. <feldrim@gmail.com>
2017-05-16 19:51:42 +0300

    Converted "ImageFormat" to "ScreenshotImageFormat"

--- Tests/Acceptance/Web.Acceptance.Tests/Steps/Hooks.cs
+++ Tests/Acceptance/Web.Acceptance.Tests/Steps/Hooks.cs
@@ -10,52 +10,54 @@ using OpenQA.Selenium.Remote;
 
 namespace SecurityEssentials.Acceptance.Tests.Web.Steps
 {
-	[Binding]
-	public class Hooks
-	{
+    [Binding]
+    public class Hooks
+    {
 
-		[BeforeFeature]
-		public static void BeforeFeature()
-		{
+        [BeforeFeature]
+        public static void BeforeFeature()
+        {
 
-			var webBrowserProxy = ConfigurationManager.AppSettings["WebBrowserProxy"].ToString();
-			IWebDriver webDriver = null;
-			if (!string.IsNullOrEmpty(webBrowserProxy))
-			{
-				var proxy = new Proxy();
-				proxy.HttpProxy = webBrowserProxy;
-				proxy.FtpProxy = webBrowserProxy;
-				proxy.SslProxy = webBrowserProxy;
-				var capabilities = new DesiredCapabilities();
-				capabilities.SetCapability(CapabilityType.Proxy, proxy);
-				webDriver = new FirefoxDriver(capabilities);
-			}
-			else
-			{
-				webDriver = new FirefoxDriver();
-			}
-			webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            var webBrowserProxy = ConfigurationManager.AppSettings["WebBrowserProxy"].ToString();
+            IWebDriver webDriver = null;
+            if (!string.IsNullOrEmpty(webBrowserProxy))
+            {
+                var proxy = new Proxy();
+                proxy.HttpProxy = webBrowserProxy;
+                proxy.FtpProxy = webBrowserProxy;
+                proxy.SslProxy = webBrowserProxy;
+                var capabilities = new DesiredCapabilities();
+                capabilities.SetCapability(CapabilityType.Proxy, proxy);
+                webDriver = new FirefoxDriver(capabilities);
+            }
+            else
+            {
+                webDriver = new FirefoxDriver();
+            }
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
             FeatureContext.Current.SetWebDriver(webDriver);
 
-			var baseUri = new Uri(ConfigurationManager.AppSettings["WebServerUrl"]);
-			FeatureContext.Current.SetBaseUri(baseUri);
+            var baseUri = new Uri(ConfigurationManager.AppSettings["WebServerUrl"]);
+            FeatureContext.Current.SetBaseUri(baseUri);
 
-		}
+        }
 
-		[AfterFeature]
-		public static void AfterFeature()
-		{
-			if (FeatureContext.Current.HasWebDriver()) FeatureContext.Current.GetWebDriver().Quit();
-		}
+        [AfterFeature]
+        public static void AfterFeature()
+        {
+            if (FeatureContext.Current.HasWebDriver()) FeatureContext.Current.GetWebDriver().Quit();
+        }
 
-		[AfterScenario]
-		public static void AfterScenario() {
-			if (ScenarioContext.Current.TestError != null && Convert.ToBoolean(ConfigurationManager.AppSettings["TakeScreenShotOnFailure"]) == true) {
-				var fileName = string.Format("{0}TestFailure-{1}.png", ConfigurationManager.AppSettings["TestScreenCaptureDirectory"].ToString(), DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss"));
-				FeatureContext.Current.GetWebDriver().TakeScreenshot().SaveAsFile(fileName, ImageFormat.Png);
-			}
-		}
+        [AfterScenario]
+        public static void AfterScenario()
+        {
+            if (ScenarioContext.Current.TestError != null && Convert.ToBoolean(ConfigurationManager.AppSettings["TakeScreenShotOnFailure"]) == true)
+            {
+                var fileName = string.Format("{0}TestFailure-{1}.png", ConfigurationManager.AppSettings["TestScreenCaptureDirectory"].ToString(), DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss"));
+                FeatureContext.Current.GetWebDriver().TakeScreenshot().SaveAsFile(fileName, ScreenshotImageFormat.Png);
+            }
+        }
 
 
-	}
+    }
 }
