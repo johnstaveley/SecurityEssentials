40a1e5e5e10d4be16c4174f680523473ef227ec5
John Staveley <john@staveley.org>
2018-03-28 09:03:53 +0100

    Changed advice on Http public key pinning

--- SecurityEssentials/Core/HttpHeaders.cs
+++ SecurityEssentials/Core/HttpHeaders.cs
@@ -27,13 +27,12 @@ namespace SecurityEssentials.Core
 #endif
 			HttpContext.Current.Response.Headers.Add("X-Content-Type-Options", "nosniff"); // SECURE: Prevent site being displayed in a different format in older browsers c.f. https://www.owasp.org/index.php/List_of_useful_HTTP_headers
 			HttpContext.Current.Response.Headers.Add("X-XSS-Protection", "1; mode=block; report=/Security/CspReporting"); // SECURE: Enable browsers anti-XSS protection and report any violations  https://www.owasp.org/index.php/XSS_(Cross_Site_Scripting)_Prevention_Cheat_Sheet
+			// SECURE: Public key pinning to be deprecated, do not use: http://www.zdnet.com/article/google-chrome-is-backing-away-from-public-key-pinning-and-heres-why/
+			HttpContext.Current.Response.Headers.Add("Public-Key-Pins", "max-age=0; includeSubDomains"); // Remove public key pinning 
+			//HttpContext.Current.Response.Headers.Add("Public-Key-Pins", "pin-sha256=\"<primary key>\"; pin-sha256=\"<backup key>\"; max-age=10000; includeSubDomains  report-uri /Security/HpkpReporting");
 #if DEBUG
 			HttpContext.Current.Response.Headers.Add("Strict-Transport-Security", "max-age=0; includeSubDomains"); // Remove HSTS header for debug
-			HttpContext.Current.Response.Headers.Add("Public-Key-Pins", "max-age=0; includeSubDomains"); // Remove public key pinning for debug
-		//HttpContext.Current.Response.Headers.Add("Public-Key-Pins-Report-Only", "pin-sha256=\"cUPcTAZWKaASuYWhhneDttWpY3oBAkE3h2+soZS7sWs=\"; pin-sha256=\"M8HztCzM3elUxkcjR2S5P4hhyBNf6lHkmjAHKhpGPWE=\"; max-age=100; includeSubDomains; report-uri /Security/HpkpReporting");
 #else
-// SECURE: TODO: Consider use of public key pinning when used with an installed TLS certificate, test with Public-Key-Pins-Report-Only
-//HttpContext.Current.Response.Headers.Add("Public-Key-Pins", "pin-sha256=\"<primary key>\"; pin-sha256=\"<backup key>\"; max-age=10000; includeSubDomains  report-uri /Security/HpkpReporting");
 // SECURE: Enforce any further requests after initial request to be made over TLS
 			HttpContext.Current.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000; includeSubDomains");
 #endif
