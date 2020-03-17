2df4ec73a56196c29e5b5cd25609564917a9daeb
John Staveley <john@staveley.org>
2020-03-17 09:43:55 +0000

    Changed method of getting ip address of agent

--- DeployPlatform/Scripts/OpenDatabaseForTesting.ps1
+++ DeployPlatform/Scripts/OpenDatabaseForTesting.ps1
@@ -1,25 +1,24 @@
 <#
 .SYNOPSIS
     Enables Azure Devops agent to connect to the sql database
+.DESCRIPTION
+	Calling this function opens a port in the firewall so that the unit tests can change the database state before running integration tests
 .NOTES
     Author: John Staveley
     Date:   17/03/2020    
 #>
 param (
-	[string] $Address, 
-	[string] $Token, 
+	[Parameter(Mandatory=$true)]
 	[string] $ResourceGroup, 
+	[Parameter(Mandatory=$true)]
 	[string] $ServerName,
+	[Parameter(Mandatory=$true)]
 	[string] $RuleName
 	)
-# Calling this function opens a port in the firewall so that the unit tests can change the database state before running integration tests
 Write-Host ("Eanble Database access for Testing")
-Write-Host "Making call to $Address at Resource Group $ResourceGroup, Server Name $ServerName and Rule Name $RuleName"
-$basicAuth = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes(("{0}:{1}" -f 'test', $Token)))
-$headers = @{Authorization=("Basic {0}" -f $basicAuth)}
-Invoke-RestMethod http://ipinfo.io/json | Select -exp ip
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$result = Invoke-RestMethod -Uri $Address -headers $headers -Method Get
-Write-Host ("IP Address " + $result.value)
-New-AzureRmSqlServerFirewallRule -ResourceGroupName $ResourceGroup -ServerName $ServerName -FirewallRuleName $RuleName -StartIpAddress "$($result.value)" -EndIpAddress "$($result.value)"
+#NB: This puts a dependency on the website http://ipinfo.io/json, but it seems to work fine
+$agentIPAddresss = Invoke-RestMethod http://ipinfo.io/json | Select -exp ip
+Write-Host ("Adding firewall rule $RuleName for Server $ServerName allowing IP Address $agentIPAddresss")
+New-AzureRmSqlServerFirewallRule -ResourceGroupName $ResourceGroup -ServerName $ServerName -FirewallRuleName $RuleName -StartIpAddress "$($agentIPAddresss)" -EndIpAddress "$($agentIPAddresss)"
 Write-Host ("Eanble Database access for Testing Complete")
