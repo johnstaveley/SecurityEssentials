781329d31e07f75b556bd68ad8b0e496c53d9fcb
John Staveley <john@staveley.org>
2020-03-24 13:59:00 +0000

    Amended clean up script after test run Put in alert for web test

--- DeployPlatform/Scripts/TearDown.ps1
+++ DeployPlatform/Scripts/TearDown.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    Removes all charging assets created by the  site Security Essentials
+    Removes all charging assets created by the Security Essentials website
 .NOTES
     Author: John Staveley
     Date:   15/03/2020    
@@ -14,7 +14,6 @@ Param(
 	[string] $SubscriptionId
 )
 
-
 Write-Host("Teardown started")
 $ErrorActionPreference = 'Continue'
 [string] $resourceGroupName = $SiteName + '-' + $EnvironmentName
@@ -25,6 +24,7 @@ $ErrorActionPreference = 'Continue'
 [string] $virtualNetworkName = $siteNameLowerCase + $EnvironmentName.ToLower()
 [string] $networkSecurityGroupName = $siteNameLowerCase + $EnvironmentName.ToLower() + '-frontend'
 [string] $applicationInsightsName = $siteNameLowerCase + $EnvironmentName.ToLower()
+[string] $webTestName = $siteNameLowerCase + $EnvironmentName.ToLower() + '-webtestisalive'
 
 # Create or check for existing resource group
 $resourceGroup = Get-AzureRmResourceGroup -Name $resourceGroupName -ErrorAction SilentlyContinue
@@ -40,6 +40,8 @@ if($resourceGroup) {
 	Remove-AzureRmResource -ResourceGroupName $resourceGroupName -ResourceName $virtualNetworkName -ResourceType Microsoft.Network/virtualNetworks -Force
 	Write-Host("Removing Network security group $networkSecurityGroupName from resource group $resourceGroupName")
 	Remove-AzureRmResource -ResourceGroupName $resourceGroupName -ResourceName $networkSecurityGroupName -ResourceType Microsoft.Network/networkSecurityGroups -Force
+	Write-Host("Removing Web Test $webTestName from resource group $resourceGroupName")
+	Remove-AzureRmResource -ResourceGroupName $resourceGroupName -ResourceName $webTestName -ResourceType microsoft.insights/webtests -Force
 	Write-Host("Removing Application Insights $applicationInsightsName from resource group $resourceGroupName")
 	Remove-AzureRmResource -ResourceGroupName $resourceGroupName -ResourceName $applicationInsightsName -ResourceType Microsoft.Insights/components -Force
 } else {
